b722ffe6b3be3b81ef7f89514cd9c323dc766d21
Hardik <hardikb.octal8@gmail.com>
2023-12-21 16:48:37 +0530

    Complete the website

--- src/components/About.js
+++ src/components/About.js
@@ -14,8 +14,8 @@ const About = () => {
   return <section id='about' className='section' ref={ref}>
     <div className='container mx-auto'>
       <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
-        <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className=' bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'> <img src={image} alt="" /></motion.div>
-        <motion.div variants={fadeIn('left', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} >
+        <motion.div variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'}   className=' bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'> <img src={image} alt="" /></motion.div>
+        <motion.div variants={fadeIn('left', 0.3)} initial='hidden' whileInView={'show'}  >
           <h2 className=' text-[40px] font-bold text-accent'>About Me.</h2>
           <h3 className='capitalize text-[30px] font-bold mb-4'>I am a Freelance front-end developer with over 5 years of expedience.  </h3>
           <p className='mb-6'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga omnis earum doloribus eligendi ipsum voluptates aliquid ducimus ut! .</p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div id='contact' className='section'>Contact</div>;
+  return (
+    <section id="contact" className="py-15 lg:section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1"
+          >
+            <div>
+              <h4 className="text-xl text-accent text-medium mb-2 tracking-wide ">
+                Get in touch
+              </h4>
+              <h2 className="uppercase text-[45px] lg:text-[90px] leading-none mb-12">
+                Let's work <br />
+                together!
+              </h2>
+            </div>
+          </motion.div>
+
+          <motion.form
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 border rounded-2xl pb-24 flex flex-col items-start  gap-y-6 p-6"
+          >
+            <input
+              type="text "
+              placeholder="Your name"
+              className="bg-transparent border-b outline-none focus:border-accent placeholder:text-white w-full py-3 transition-all "
+            />
+            <input
+              type="email "
+              required
+              placeholder="Your email"
+              className="bg-transparent border-b outline-none focus:border-accent placeholder:text-white w-full py-3 transition-all "
+            />
+            <textarea
+              placeholder="Your massage"
+              className="pl touch-pan-left bg-transparent resize-none mb-12  border-b outline-none focus:border-accent placeholder:text-white w-full py-12 transition-all "
+            ></textarea>
+            <button className="btn btn-lg">Send massage </button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
